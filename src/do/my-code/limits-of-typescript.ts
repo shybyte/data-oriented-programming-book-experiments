c2fda1aa87111bebbadb69b6807463cb432ee952
shybyte <shybyte@users.noreply.github.com>
2021-03-22 11:13:38 +0100

    limits-of-typescript.ts

---
+++ src/do/my-code/limits-of-typescript.ts
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import { Author } from '../data-model';
+
+const author: Author = {
+  id: 'author-id',
+  name: 'Author Name',
+  bookIsbns: ['123', '234']
+}
+
+function getPropertyKeyFromEventName<T extends keyof Author>(prop: `${T}Changed`): T {
+  return prop.replace('Changed', '') as T;
+}
+
+// const prop = getPropertyKeyFromEventName('unknownChanged'); // Does not compile, because 'unknown' is no property of Author
+const prop = getPropertyKeyFromEventName('nameChanged'); // returns 'name'
+const partialAuthor = _.pick(author, 'id', prop);
+
+// Compiles because TypeScript knows that name can't be undefined,
+// because the getPropertyKeyFromEventName('nameChanged') will return 'name' for the input 'nameChanged'
+// and pick will select the 'name' property
+const name = partialAuthor.name.toLowerCase();
+
+// Does not compile because TypeScript knows that the pick function has ignored the bookIsbns
+// const bookIsbn = partialAuthor.bookIsbn
+
+
